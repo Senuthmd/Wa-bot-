bdc78d16fc0a527ded30a1f1376e76ffbd5c447a
Senuthmd <mdsenuthdinsara@gmail.com>
2025-09-19 09:51:41 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+{
+  "name": "whatsapp-bot",
+  "version": "1.0.0",
+  "description": "A simple WhatsApp bot",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "dependencies": {
+    "qrcode-terminal": "^0.12.0",
+    "whatsapp-web.js": "^1.23.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  },
+  "keywords": ["whatsapp", "bot"],
+  "author": "Your Name",
+  "license": "MIT"
+}
